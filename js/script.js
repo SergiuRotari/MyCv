45b366b9321caf58971325184e85a816e25095dc
SergiuRotari <sergiu.rotari.pro@gmail.com>
2021-12-10 17:01:12 +0200

    skill-corrected

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
-$(document).ready(function(){
+$(document).ready(function () {
 
-  $('#menu').click(function(){
+  $('#menu').click(function () {
     $(this).toggleClass('fa-times');
     $('header').toggleClass('toggle');
   });
 
-  $(window).on('scroll load',function(){
+  $(window).on('scroll load', function () {
 
     $('#menu').removeClass('fa-times');
     $('header').removeClass('toggle');
 
-    if($(window).scrollTop() > 0){
+    if ($(window).scrollTop() > 0) {
       $('.top').show();
-    }else{
+    } else {
       $('.top').hide();
     }
 
@@ -20,16 +20,16 @@ $(document).ready(function(){
 
   // smooth scrolling 
 
-  $('a[href*="#"]').on('click',function(e){
+  $('a[href*="#"]').on('click', function (e) {
 
     e.preventDefault();
 
     $('html, body').animate({
 
-      scrollTop : $($(this).attr('href')).offset().top,
+      scrollTop: $($(this).attr('href')).offset().top,
 
     },
-      500, 
+      500,
       'linear'
     );
 
@@ -37,33 +37,32 @@ $(document).ready(function(){
 
 });
 var settings = {
-  //height of sphere container
   height: 700,
-  //width of sphere container
-  width: 700,
-  //radius of sphere
-  radius: 250,
-  
-  //rotation speed
-  speed: 2,
-  //sphere rotations slower
-  slower: 0.5,
-  //delay between update position
-  timer: 5,
-  //dependence of a font size on axis Z
-  fontMultiplier: 35,
-  //tag css stylies on mouse over
-  hoverStyle: {
-      border: 'none',
-      color: '#0000'
-  },
-  //tag css stylies on mouse out
-  mouseOutStyle: {
-      border: '',
-      color: '#0000'
-  }
-  };
 
-  $(document).ready(function(){
-      $('#tagcloud').tagoSphere(settings);
-  });
+  width: 700,
+
+  radius: 250,
+
+
+  speed: 2,
+
+  slower: 0.5,
+
+  timer: 5,
+
+  fontMultiplier: 35,
+
+  hoverStyle: {
+    border: 'none',
+    color: '#0000'
+  },
+
+  mouseOutStyle: {
+    border: '',
+    color: '#0000'
+  }
+};
+
+$(document).ready(function () {
+  $('#tagcloud').tagoSphere(settings);
+});
